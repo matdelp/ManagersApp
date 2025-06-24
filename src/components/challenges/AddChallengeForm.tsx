67383ccd6d9237efdff63cef608415b6b95ecb43
matdelp <mathilde.delpeuch@gmail.com>
2025-06-24 18:39:59 +0200

    feat: add testcases handeling

--- src/components/challenges/AddChallengeForm.tsx
+++ src/components/challenges/AddChallengeForm.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
+import { FaRegTrashAlt } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { langs } from "@uiw/codemirror-extensions-langs";
 import CodeMirror from "@uiw/react-codemirror";
-import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { SubmitHandler, useForm, useFieldArray } from "react-hook-form";
 import { SimpleMdeReact } from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import {
@@ -23,21 +24,51 @@ import {
 } from "@/components/ui/select";
 import { useFontSizeStore } from "@/store/useFontSizeStore";
 
+interface TestCase {
+  id: number;
+  type: string;
+  argument: string;
+  value: number;
+  output: number;
+  weight: number;
+}
+
+interface ChallengeFormData {
+  title: string;
+  category: string;
+  level: "Easy" | "Moderate" | "Hard";
+  bio: string;
+  function: string;
+  items: TestCase[];
+}
+
 export const AddChallengeForm: React.FC = () => {
   const [code, setCode] = useState(`// Type your code here`);
   const [language, setLanguage] = useState<string>("javascript");
+
   const { size, changeSize } = useFontSizeStore();
 
-  const form = useForm();
-  const onCreate: SubmitHandler<FieldValues> = (values) => {
+  const form = useForm<ChallengeFormData>({
+    defaultValues: {
+      title: "Title",
+      category: "unknown",
+      level: "Easy",
+      bio: "",
+      function: "Function",
+      items: [],
+    },
+  });
+  const { fields, append, remove } = useFieldArray({
+    control: form.control,
+    name: "items",
+  });
+  const onCreate: SubmitHandler<ChallengeFormData> = (values) => {
     console.log(values);
   }; // Change to my custom type of data ?
   const handleChange = (value: string) => {
     setCode(value); //temporary
   };
-  const handleClick = () => {
-    console.log("Code is:", code);
-  }; //temporary
+
   return (
     <Form {...form}>
       <form
@@ -87,7 +118,7 @@ export const AddChallengeForm: React.FC = () => {
                 <FormLabel>level*</FormLabel>
                 <Select onValueChange={field.onChange} value={field.value}>
                   <FormControl>
-                    <SelectTrigger>
+                    <SelectTrigger className="w-full">
                       <SelectValue placeholder="Select level" />
                     </SelectTrigger>
                   </FormControl>
@@ -104,11 +135,14 @@ export const AddChallengeForm: React.FC = () => {
           <FormField
             control={form.control}
             name="bio"
-            render={() => (
+            render={({ field }) => (
               <FormItem>
                 <FormLabel>Description</FormLabel>
                 <FormControl>
-                  <SimpleMdeReact />
+                  <SimpleMdeReact
+                    value={field.value}
+                    onChange={field.onChange}
+                  />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -179,18 +213,142 @@ export const AddChallengeForm: React.FC = () => {
                   }
                   onChange={handleChange}
                   style={{
-                    fontSize: size
+                    fontSize: size,
                   }}
                 />
                 <Button
                   type="button"
                   className="bg-main-500 text-lg p-5 cursor-pointer absolute right-3 -bottom-10"
-                  style={{ zIndex: 10 }}
-                  onClick={handleClick}
+                  onClick={() =>
+                    append({
+                      id: Date.now(),
+                      type: "number",
+                      argument: "a",
+                      value: 10,
+                      output: 10,
+                      weight: 0.8,
+                    })
+                  }
                 >
                   +
                 </Button>
-              </div>
+              </div>{" "}
+              <h2 className="text-xl py-5">
+                Tests<span>*</span>
+              </h2>
+              {fields.map((field, index) => (
+                <div key={field.id} className="flex gap-2 w-full">
+                  <div className="flex items-center">
+                    <Button
+                      type="button"
+                      aria-label="Remove test case"
+                      className="bg-red-500 cursor-pointer"
+                      onClick={() => remove(index)}
+                    >
+                      <FaRegTrashAlt />
+                    </Button>
+                  </div>
+                  <div className="flex flex-wrap gap-2 w-full">
+                    <FormField
+                      control={form.control}
+                      name={`items.${index}.type`}
+                      render={({ field }) => (
+                        <FormItem className="flex-grow basis-1/3">
+                          <FormLabel>Type</FormLabel>
+                          <FormControl>
+                            <Select
+                              value={field.value}
+                              onValueChange={field.onChange}
+                            >
+                              <SelectTrigger className="w-full">
+                                <SelectValue placeholder="Select type" />
+                              </SelectTrigger>
+                              <SelectContent>
+                                <SelectItem value="number">number</SelectItem>
+                                <SelectItem value="string">string</SelectItem>
+                              </SelectContent>
+                            </Select>
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+
+                    <FormField
+                      control={form.control}
+                      name={`items.${index}.argument`} // <-- unique name
+                      render={({ field }) => (
+                        <FormItem className="flex-grow basis-1/3">
+                          <FormLabel>Name</FormLabel>
+                          <FormControl>
+                            <Input
+                              className="bg-background-100 text-main-700 py-5"
+                              type="string"
+                              placeholder="argument"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+
+                    <FormField
+                      control={form.control}
+                      name={`items.${index}.value`} // <-- unique name
+                      render={({ field }) => (
+                        <FormItem className="flex-grow basis-1/3">
+                          <FormLabel>Value</FormLabel>
+                          <FormControl>
+                            <Input
+                              className="bg-background-100 text-main-700 py-5"
+                              type="number"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+
+                    <FormField
+                      control={form.control}
+                      name={`items.${index}.output`} // <-- unique name
+                      render={({ field }) => (
+                        <FormItem className="flex-grow basis-1/3">
+                          <FormLabel>Output</FormLabel>
+                          <FormControl>
+                            <Input
+                              className="bg-background-100 text-main-700 py-5"
+                              type="number"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+
+                    <FormField
+                      control={form.control}
+                      name={`items.${index}.weight`} // <-- unique name
+                      render={({ field }) => (
+                        <FormItem className="w-1/2">
+                          <FormLabel>Weight</FormLabel>
+                          <FormControl>
+                            <Input
+                              className="bg-background-100 text-main-700 py-5"
+                              type="number"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
         </div>
