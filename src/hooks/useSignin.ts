9f3324060dc4f4181c9d7974104f825c2c8235dc
matdelp <mathilde.delpeuch@gmail.com>
2025-07-28 10:56:19 +0200

    feat: add token storage in cookies and local store

--- src/hooks/useSignin.ts
+++ src/hooks/useSignin.ts
@@ -1,11 +1,14 @@
+import { useAuthStore } from "@/store/useAuthStore";
 import { ManagerLogin } from "@/types/managerLogin";
 import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
+import Cookies from "js-cookie";
 
 const FETCH_URL = "/auth/managers/login";
 
 export const useManagerLogin = () => {
   const router = useRouter();
+  const setToken = useAuthStore((state) => state.setToken);
 
   return useMutation<void, Error, ManagerLogin>({
     mutationFn: async (data) => {
@@ -20,6 +23,14 @@ export const useManagerLogin = () => {
       if (!response.ok) {
         throw new Error("Login failed");
       }
+      const resData = await response.json();
+      const token = resData.token;
+      setToken(token);
+      Cookies.set("auth_token", token, {
+        expires: 7, // days
+        secure: true,
+        sameSite: "lax",
+      });
       return;
     },
     onSuccess: () => {
